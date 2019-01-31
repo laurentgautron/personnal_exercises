6a97a1c709a2b0be3c2e2ef5ed59c753a6fd4186
laurent GAUTRON <laurentgautron.sfr.fr>
2019-01-31 22:59:18 +0100

    optimise rock, paper, scissors and begin game_guess_one

--- on_practicepython/rock_paper_scissors.py
+++ on_practicepython/rock_paper_scissors.py
@@ -3,22 +3,23 @@ message of congratulations to the winner, and ask if the players want to start a
 """
 
 to_continue = ""
+list_item = {1:"rock", 2:"paper", 3:"scissors"}
 while True:
     if to_continue == "n":
         print("Ok bye")
         break
-    gamer1 = input("First gamer must choose :\n- 1 = rock\n- 2 = paper\n- 3 = scissors\nYour choice: ")
-    gamer2 = input("second gamer must choose :\n- 1 = rock\n- 2 = paper\n- 3 = scissors\nYour choice: ")
+    gamer1 = int(input("First gamer must choose :\n- 1 = rock\n- 2 = paper\n- 3 = scissors\nYour choice: "))
+    gamer2 = int(input("second gamer must choose :\n- 1 = rock\n- 2 = paper\n- 3 = scissors\nYour choice: "))
     if gamer1 == gamer2:
         print("nobody win !!!")
     elif (gamer1 == 1) and (gamer2 == 3):
-        print("The first gamer wins !!!")
+        print("The {} wins !!!".format(list_item[gamer1]))
     elif (gamer1 == 2) and (gamer2 == 1):
-        print("The first gamer wins !!!")
+        print("The {} wins !!!".format(list_item[gamer1]))
     elif (gamer1 == 3) and (gamer2 == 2):
-        print("The first gamer wins !!!")
+        print("The {} wins !!!".format(list_item[gamer1]))
     else:
-        print("The second gamer wins !!!")
+        print("The {} wins !!!".format(list_item[gamer2]))
     to_continue = input(" an other game ? (y/n): ")
     while to_continue not in ("n", "y"):
         print("you tap: ", to_continue)
