ccceb96a23cb0ffb47fc222014f7284fe9a327f2
laurent GAUTRON <laurentgautron.sfr.fr>
2019-01-31 23:46:06 +0100

    optimised game_guess_one

--- on_practicepython/game_guess_one.py
+++ on_practicepython/game_guess_one.py
@@ -10,11 +10,10 @@ user_number = input("guess the number (or type 'exit' if you want to quit): ")
 while user_number not in (number, 'exit'):
     if user_number == "exit":
         break
-    elif user_number < number:
+    if user_number < number:
         print("it's greater")
-    elif user_number > number:
+    else:
         print("it's smaller")
     user_number = input("try again: ")
-    print(user_number not in (number, 'exit'))
     counter += 1
 print("you win in {} try !!".format(counter))
