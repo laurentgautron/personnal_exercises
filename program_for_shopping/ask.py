8e7184a201a74ba5aba3fa228937c246a2d254fc
Laurent GAUTRON <lg@Laurents-iMac.local>
2019-04-05 18:05:25 +0200

    add class for all questions in program, must change other classes

--- program_for_shopping/ask.py
+++ program_for_shopping/ask.py
@@ -1,27 +1,71 @@
 import os
 
+
 class Ask:
 
-    @staticmethod
-    def ask_liste(liste):
-        choice = len(liste) + 1
-        while choice not in range(1, len(list) + 1):
-            os.system('clear')
-            choice = input('rentrer un nombre parmi la liste qui s\'affiche ou 0 pour enregistrer un nouvel élément')
+    def ask_liste(self, question, liste):
+        no_choosed = True
+        choice = input(question)
+        while no_choosed:
+            choice = input(question)
             display(liste)
+            try:
+                int(choice)
+            except ValueError:
+                new_item = self.ask_string('vous voulez rajouter ' + choice + 'dans la liste? ', yn=True)
+                if new_item:
+                    no_choosed = False
+                    # new in list(choice)
+            else:
+                while choice not in range(1, len(liste) + 1):
+                    os.system('clear')
+                    choice = input('rentrer un nombre parmi la liste qui s\'affiche ou 0 pour enregistrer un nouvel élément')
         return choice
 
     @staticmethod
-    def ask_string(question, yn = False):
+    def ask_string(question, yn=False):
         if yn:
             choice = 'a'
             while choice not in ('o', 'n'):
-                choice = input(question + (' répondez oui(o) ou non(n) '))
+                choice = input(question + ' répondez oui(o) ou non(n) ')
+            return True
         else:
             choice = input(question)
-        return choice
+            return choice
 
     @staticmethod
-    def ask_price_weight_quatity(question, price=False, weight=False, price=False):
-        cas price = True:
-            try float(input(question).replace(',', '.')):
+    def ask_number(question, nbarticle=False, weight=False, price=False, way_number=False):
+        number_entered = False
+        while not number_entered:
+            if nbarticle:
+                choice = input(question)
+                try:
+                    int(choice)
+                except ValueError:
+                    print("entrer un nombre entier pour le nombre d'articles ")
+                else:
+                    return int(choice)
+            elif weight:
+                choice = input(question)
+                try:
+                    float(choice)
+                except ValueError:
+                    print('vous devez rentrer un nombre pour le poids')
+                else:
+                    if (float(choice)*1000).is_integer():
+                        return float(choice)
+                    else:
+                        print('seulement 3 chiffres après la virgule')
+            elif price:
+                choice = input(question)
+                try:
+                    float(choice)
+                except ValueError:
+                    print('vous devez rentrer un prix pour cet achat')
+                else:
+                    choice = str(float(choice)*10)
+                    if len(choice) > 3:
+                        print('seulement trois chiffres avant une éventuelle virgule et deux chiffres autorisés après\
+                              la virgule si besoin')
+                    else:
+                        return float(choice)
