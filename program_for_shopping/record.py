9c145740259bdd27a0630ad31fbcd1732c8304e5
LaurentG <laurentg@LaurentGs-iMac.local>
2019-09-19 20:01:07 +0200

    new algo for shopping

--- program_for_shopping/record.py
+++ program_for_shopping/record.py
@@ -16,6 +16,6 @@ class Record:
                 store_id = Store.insert()
                 Purchase.insert(store_id)
             last_purchase = Purchase.find_last_purchase()
-            product_id = Product.insert(last_purchase())
+            product_id = Product.record_product(last_purchase())
             PurchaseProduct.insert(last_purchase, product_id)
             purch = Ask.ask_string('voulez-vous enregistrer un autre achat? ', yn=True)
